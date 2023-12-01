9b09fbe61c165b2026ec93c5696c8cad2b884a6a
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2023-12-01 17:40:03 +0500

    Initial Commit

---
+++ swiftbookAppStudyHW2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftbookAppStudyHW2.2
+//
+//  Created by MacOS on 01.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
